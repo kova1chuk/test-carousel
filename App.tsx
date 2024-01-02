42a5bae97451da9c74b2f9532bea44864b255adf
kovalchuk <mail.to.kovalchuk@gmail.com>
2024-01-02 13:31:01 +0200

    Screen Start

--- App.tsx
+++ App.tsx
@@ -1,118 +1,170 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/react-in-jsx-scope */
+import {useRef} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
+  SafeAreaView,
+  Dimensions,
   View,
+  Text,
+  Button,
+  Touchable,
+  TouchableOpacity,
+  Animated,
 } from 'react-native';
-
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  GestureHandlerRootView,
+  TapGestureHandler,
+} from 'react-native-gesture-handler';
+import {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+import Carousel from 'react-native-reanimated-carousel';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const width = Dimensions.get('window').width;
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const carouselRef = useRef(null);
+  const offsetX = useSharedValue(0);
+  const tapX = useSharedValue(0);
+  const isSwiping = useSharedValue(false);
+
+  const onDragBy10Pixels = () => {
+    const targetX = offsetX.value + 10;
+    isSwiping.value = true;
+
+    // Simulate swipe by animating to the new position
+    offsetX.value = withSpring(
+      targetX,
+      {damping: 2, stiffness: 80},
+      isFinished => {
+        if (isFinished) {
+          isSwiping.value = false;
+        }
+      },
+    );
+  };
+
+  const renderCarouselItem = ({index}) => (
+    <View style={{flex: 1, borderWidth: 1, justifyContent: 'center'}}>
+      <Text style={{textAlign: 'center', fontSize: 30}}>{index}</Text>
+    </View>
+  );
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, context) => {
+      context.startX = offsetX.value;
+      tapX.value = offsetX.value;
+    },
+    onActive: (event, context) => {
+      if (!isSwiping.value) {
+        const diff = event.translationX - context.startX;
+        tapX.value = context.startX + diff;
+      }
+    },
+    onEnd: () => {
+      // Check if the tap gesture initiated a swipe
+      if (Math.abs(tapX.value - offsetX.value) >= 10) {
+        runOnJS(onDragBy10Pixels)();
+      }
+    },
+  });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: offsetX.value}],
+    };
+  });
+
+  setTimeout(() => {
+    carouselRef.current?.scrollTo({count: 0.4, animated: true});
+  }, 1000);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
+    <GestureHandlerRootView style={{flex: 1}}>
+      <SafeAreaView style={backgroundStyle}>
+        <Carousel
+          ref={carouselRef}
+          // loop
+          width={width}
+          height={width / 2}
+          // autoPlay={true}
+          data={[...new Array(6).keys()]}
+          scrollAnimationDuration={1000}
+          onSnapToItem={index => console.log('current index:', index)}
+          renderItem={({index}) => (
+            <View
+              style={{
+                flex: 1,
+                borderWidth: 1,
+                justifyContent: 'center',
+              }}>
+              <Text style={{textAlign: 'center', fontSize: 30}}>{index}</Text>
+            </View>
+          )}
+        />
+        <Carousel
+          // loop
+          width={width}
+          height={width / 2}
+          // autoPlay={true}
+          data={[...new Array(6).keys()]}
+          scrollAnimationDuration={1000}
+          onSnapToItem={index => console.log('current index:', index)}
+          renderItem={({index}) => (
+            <View
+              style={{
+                flex: 1,
+                borderWidth: 1,
+                justifyContent: 'center',
+              }}>
+              <Text style={{textAlign: 'center', fontSize: 30}}>{index}</Text>
+            </View>
+          )}
+        />
+        <TouchableOpacity
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: 50,
+            backgroundColor: 'green',
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+          <Text
+            onPress={() => {
+              // startAnimation();
+            }}>
+            Text
+          </Text>
+        </TouchableOpacity>
+        {/* <View>
+          <TapGestureHandler onGestureEvent={gestureHandler}>
+            <Animated.View>
+              <Carousel
+                ref={carouselRef}
+                data={[...new Array(6).keys()]}
+                renderItem={renderCarouselItem}
+                // sliderWidth={200}
+                // itemWidth={150}
+                // containerCustomStyle={{overflow: 'hidden'}}
+              />
+            </Animated.View>
+          </TapGestureHandler>
+        </View> */}
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+// const styles = StyleSheet.create({});
 
 export default App;
